443553fa56c416dc762e2c05d895b5924ac1c015
Cameron Robinson <cameron.robinson804@gmail.com>
2023-01-31 21:29:45 +0000

    Done

--- src/main/java/IfStatement.java
+++ src/main/java/IfStatement.java
@@ -1,5 +1,5 @@
 
-public class IfStatement {
+public class IfStatement{ 
     /**
      * An if statement uses a conditional statement (within parentheses) to determine if the code
      * within {curly braces} should run.
@@ -17,9 +17,16 @@ public class IfStatement {
      * @param bool a true/false value that determines if x or y is to be returned.
      * @param x first number to be compared.
      * @param y second number to be compared.
-     * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
-    }
+   
+    
+   
+   if (bool == true) {
+ return x;
+   }
+   else {
+ return y;
+   }
+  }
 }
